443ebdf1bb68ef5f44c2a4bf21b1f56da2c76e97
Daniel Plundrich <dan.plundrich@gmail.com>
2022-07-16 22:38:29 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showFunction() {
+    let x = document.querySelector(".links")
+    if (x.style.display === "block"){
+        x.style.display = "none"
+    }else{
+        x.style.display = "block"
+    }
+}
